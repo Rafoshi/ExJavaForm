8e807cb3f28f5a20b4b87940081802a7b2a60f29
Rafoshi <rafa.ioshi@gmail.com>
2023-04-17 14:17:30 -0300

    Só falta getDados Viagem

--- src/br/fiap/viagem/Viagem.java
+++ src/br/fiap/viagem/Viagem.java
@@ -1,6 +1,7 @@
 package br.fiap.viagem;
 
 import br.fiap.carga.Carga;
+import br.fiap.cliente.Cliente;
 
 public class Viagem {
     private double capacidade = 10000;
@@ -10,7 +11,32 @@ public class Viagem {
     public Viagem(Carga[] carga){
         this.carga = carga;
         
-        
+    }
+
+    public String getDados(){
+        String dados = "Carga";
+        return "";
+    }
+
+    public boolean permitidoReservar(){
+        return index != 20;
+    }
+
+    public double capacidadeReservada(){
+
+        return index;
+    }
+
+    public Carga reservar(){
+        Cliente rafael = new Cliente(232191112, "Rafael");
+        Carga carga = new Carga("Av Manoel Pedro Pimentel", rafael);
+
+        if(permitidoReservar()){
+            this.carga[index] = carga;
+            index++;
+        }
+
+        return carga;
     }
     
 }
